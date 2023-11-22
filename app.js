63fb62498605b8fa92ec2afcf52f26db964c0563
Maria <maria123321borovikova@gmail.com>
2023-11-22 10:51:19 +0100

    main

--- app.js
+++ app.js
@@ -65,16 +65,24 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 document.addEventListener('scroll', () => {
+    const section1 = document.getElementById('section1'); 
     const section2 = document.getElementById('section2');
+    const section3 = document.getElementById('section3');
+    const section1Top = section1.getBoundingClientRect().top;
     const section2Top = section2.getBoundingClientRect().top;
+    const section3Top = section3.getBoundingClientRect().top;
     const viewportHeight = window.innerHeight;
 
-    if (section2Top <= viewportHeight / 2) {
-        document.body.style.backgroundColor = 'rgb(21, 21, 21)';
-    } else {
+    if (section1Top <= viewportHeight && section1Top >= 0) {
         document.body.style.backgroundColor = ''; 
+    } else if (section2Top <= viewportHeight && section2Top >= 0) {
+        document.body.style.backgroundColor = 'rgb(21, 21, 21)';
+    } else if (section3Top <= viewportHeight && section3Top >= 0) {
+        document.body.style.backgroundColor = 'rgb(44,4,12)'; 
     }
 });
+
+
 
 
 
---
+++ translation.js
@@ -0,0 +1,44 @@
+export const translations = {
+    "title": {
+        "en": "Shaping Ideas into Reality",
+        "es": "Dando Forma a las Ideas en Realidad"
+    },
+    "section1": {
+        "name": {
+            "en": "Maria Borovikova",
+            "es": "Maria Borovikova"
+        },
+        "thumbnailAlt": {
+            "en": "Image",
+            "es": "Imagen"
+        }
+    },
+    "section2": {
+        "aboutMe": {
+            "title": {
+                "en": "About Me",
+                "es": "Sobre Mí"
+            },
+            "description": {
+                "en": "Hello! I’m Maria, a passionate and creative Front-End Developer with a flair for crafting engaging and user-friendly websites. With 2 years of experience in the industry, I specialize in bringing designs to life using the latest web technologies.",
+                "es": "¡Hola! Soy Maria, una desarrolladora Front-End apasionada y creativa con un talento para crear sitios web atractivos y fáciles de usar. Con 2 años de experiencia en la industria, me especializo en dar vida a los diseños utilizando las últimas tecnologías web."
+            }
+        }
+    },
+    "section3": {
+        "projects": {
+            "title": {
+                "en": "Projects",
+                "es": "Proyectos"
+            },
+            "description": {
+                "en": "Hello! I’m Maria, a passionate and creative Front-End Developer with a flair for crafting engaging and user-friendly websites. With 2 years of experience in the industry, I specialize in bringing designs to life using the latest web technologies.",
+                "es": "¡Hola! Soy Maria, una desarrolladora Front-End apasionada y creativa con un talento para crear sitios web atractivos y fáciles de usar. Con 2 años de experiencia en la industria, me especializo en dar vida a los diseños utilizando las últimas tecnologías web."
+            }
+        }
+    },
+    "footer": {
+        "en": "",
+        "es": ""
+    }
+}
