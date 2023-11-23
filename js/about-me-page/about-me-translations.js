3cbac7f936547b29b30e619c08d764430183facd
Maria <maria123321borovikova@gmail.com>
2023-11-23 19:25:53 +0100

    responsive about me

--- js/about-me-page/about-me-translations.js
+++ js/about-me-page/about-me-translations.js
@@ -1,12 +1,12 @@
 const translations = {
     en: {
       aboutTitle: "About Me",
-      aboutText: "I excel in crafting user interfaces that are not only visually appealing but also highly functional and user-friendly. My expertise as a front-end developer lies in creating seamless and intuitive experiences for website visitors.",
+      aboutText: "I excel in crafting user interfaces that are not only intuitive and aesthetically pleasing but also highly functional. My proficiency spans a diverse range of programming languages, frameworks, and design tools, enabling me to transform my designs into efficient and maintainable implementations. Committed to continuous improvement, I actively seek out the latest trends in UI/UX and web development to enhance my skills and stay ahead in the field",
       downloadButtonText: "Download My PDF",
     },
     es: {
       aboutTitle: "Sobre mí",
-      aboutText: "Me destaco en la creación de interfaces de usuario que no sólo son visualmente atractivas sino también altamente funcionales y fáciles de usar. Mi experiencia como desarrollador front-end radica en crear experiencias fluidas e intuitivas para los visitantes de un sitio web.",
+      aboutText: "Me destaco en la creación de interfaces de usuario que no sólo son intuitivas y estéticamente agradables sino también altamente funcionales. Mi dominio abarca una amplia gama de lenguajes de programación, marcos y herramientas de diseño, lo que me permite transformar mis diseños en implementaciones eficientes y fáciles de mantener. Comprometido con la mejora continua, busco activamente las últimas tendencias en UI/UX y desarrollo web para mejorar mis habilidades y mantenerme a la vanguardia en el campo.",
       downloadButtonText: "Descargar mi PDF",
     }
   };
